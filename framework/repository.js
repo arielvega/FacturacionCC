79c121d33ec89c49959120fe50cf1c3a45351cab
luisvega <luisvega@uagrm.edu.bo>
2020-03-05 22:32:23 -0400

    creando framework

---
+++ framework/repository.js
@@ -0,0 +1,19 @@
+class Repository {
+    get(id){
+        throw new Error("Sin implementar!!!");
+    }
+    
+    save(data){
+        throw new Error("Sin implementar!!!");
+    }
+    
+    list(){
+        throw new Error("Sin implementar!!!");
+    }
+    
+    delete(id){
+        throw new Error("Sin implementar!!!");
+    }
+}
+
+
