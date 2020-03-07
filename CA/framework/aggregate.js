370944295751bc28b6076d3d385bc916857df269
luisvega <luisvega@uagrm.edu.bo>
2020-03-07 03:12:02 -0400

    agregando framework

---
+++ CA/framework/aggregate.js
@@ -0,0 +1,90 @@
+
+var Test = require('../domain/valueobjects/tester.js');
+
+class IInternalEventHandler {
+    handle(event){
+        throw new Error("Sin implementar!!!");
+    }
+}
+
+class Entity extends IInternalEventHandler
+{
+
+    constructor() {
+        super();
+        this._id = 0;
+    }
+
+    _setApplier(applier) {
+        this._applier = applier;
+    }
+
+    _applier(event) {
+        throw new Error("Sin implementar!!!");
+    }
+
+    _When(event) {
+        throw new Error("Sin implementar!!!");
+    }
+
+    _Apply(event) {
+        this._When(event);
+        this._applier(event);
+    }
+
+    get Id() {
+        return this._id;
+    }
+
+    handle(event) {
+        this._When(event);
+    }
+}
+
+
+class AggregateRoot extends IInternalEventHandler {
+
+    constructor() {
+        super();
+        this._id = 0;
+        this._changes = [];
+    }
+
+    _ValidateStatus() {
+        throw new Error("Sin implementar!!!");
+    }
+
+    _When(event) {
+        throw new Error("Sin implementar!!!");
+    }
+
+    _Apply(event) {
+        this._When(event);
+        this._applier(event);
+        this._changes[this._changes.length] = event;
+    }
+
+    getChanges() {
+        this._changes.slice();
+    }
+
+    clearChanges() {
+        this._changes.Clear();
+    }
+
+    _ApplyToEntity(entity, event) {
+        entity = Test.isA(IInternalEventHandler, entity);
+        entity.Handle(event);
+    }
+
+    get Id() {
+        return this._id;
+    }
+
+    handle(event) {
+        this._When(event);
+    }
+}
+
+module.exports = {IInternalEventHandler, Entity, AggregateRoot};
+
---
+++ CA/framework/valueobject.js
@@ -0,0 +1,8 @@
+class Value {
+    checkVality(data){
+        throw new Error("Sin implementar!!!");
+    }
+}
+
+module.exports = {Value};
+
